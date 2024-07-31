898a8e4c00bc920deda41c6d66ecc73a6212ede3
francisco <maqcontacto@gmail.com>
2024-07-31 11:06:53 -0300

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+
+import styles from './Navbar.module.css'
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      
+       <h1>Esto Es</h1>
+    
+    </nav>
+  )
+}
